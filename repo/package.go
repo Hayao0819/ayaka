75c2a6d19aa80f0ab4afb88848e0cdd3b4a4a8f8
hayao <hayao@fascode.net>
2023-11-19 03:45:55 +0900

    Update: Add GetPkgFilePath

--- repo/package.go
+++ repo/package.go
@@ -1,6 +1,10 @@
 package repo
 
 import (
+	"bytes"
+	"os/exec"
+	"strings"
+
 	"github.com/Hayao0819/ayaka/builder"
 	"github.com/Morganamilo/go-srcinfo"
 )
@@ -13,4 +17,16 @@ type Package struct {
 func (p *Package) Build(method string, target builder.Target) error {
 	builder := builder.GetBuilder(method)
 	return builder.Build(p.Path, &target)
+}
+
+func (p *Package) GetPkgFilePath() string {
+	stdout := new(bytes.Buffer)
+	cmd := exec.Command("makepkg", "--packagelist")
+	cmd.Dir = p.Path
+	cmd.Stdout = stdout
+	err := cmd.Run()
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSpace(stdout.String())
 }
